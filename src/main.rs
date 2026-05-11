51fe8c778ab9b537b15e3ace1635cd5f66397d6b
Craig Jilbert <cjilbert@gmail.com>
2026-05-11 15:00:14 +1000

    Increase default window size to 1200x900 to fit all UI content without scrolling

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,16 @@ mod ui;
 
 use config::AppConfig;
 use std::fs;
+use eframe::egui;
 
 fn main() -> eframe::Result<()> {
     let config: AppConfig = toml::from_str(&fs::read_to_string("lotto_config.toml").unwrap()).unwrap();
-    eframe::run_native("Lotto Sim", eframe::NativeOptions::default(), Box::new(|_cc| Ok(Box::new(simulation::LotteryApp::new(config)))))
+    eframe::run_native(
+        "Lotto Sim",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_inner_size([1200.0, 900.0]),
+            ..Default::default()
+        },
+        Box::new(|_cc| Ok(Box::new(simulation::LotteryApp::new(config)))),
+    )
 }
